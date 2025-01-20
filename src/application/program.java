eb142d8d8753964fe25da4cf92e70473894a80f1
Carlos Oliveira <caduolivera17@gmail.com>
2025-01-20 17:21:49 -0300

    Seller entity

--- src/application/program.java
+++ src/application/program.java
@@ -1,12 +1,17 @@
 package application;
 
-import model.entities.Department;
+import java.util.Date;
 
-public class program {
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
 
     public static void main(String[] args) {
         
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
